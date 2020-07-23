bef2d24d0dd589eec5eaca1c2c00274c0cac5c97
Prakashaditya <61825990+prakashaditya13@users.noreply.github.com>
2020-07-23 20:01:54 +0530

    Add files via upload

---
+++ payment_gateway/index.php
@@ -0,0 +1,15 @@
+<?php 
+require_once 'config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="<?php eval(base64_decode('CiBlY2hvICRMSU5LOyA=')); ?>" target="_blank">Click to navigate</a>
+</body>
+</html>
